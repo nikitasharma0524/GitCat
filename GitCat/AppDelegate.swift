ae62923889dfc7f7756d7ceff983209d7ad0a969
Nikita Sharma <nikitasharma0524@gmail.com>
2025-07-21 20:34:43 +0530

    GitCat - code.

---
+++ GitCat/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  GitCat
+//
+//  Created by Nikita on 21/07/25.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        showCatIcon()
+
+        // Hide after 3 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.hideCatIcon()
+            NSApp.terminate(nil) // Exit the app
+        }
+    }
+
+    func showCatIcon() {
+        let icon = NSImage(named: "cat") ?? NSImage(systemSymbolName: "tortoise.fill", accessibilityDescription: nil)
+        icon?.isTemplate = false
+
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        statusItem?.button?.image = icon
+    }
+
+    func hideCatIcon() {
+        if let item = statusItem {
+            NSStatusBar.system.removeStatusItem(item)
+            statusItem = nil
+        }
+    }
+}
